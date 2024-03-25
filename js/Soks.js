e212d38bfba6c2b7f158de291d2d51f712b91d78
Dark-StarStudios <146312613+Dark-StarStudios@users.noreply.github.com>
2024-03-25 19:30:01 +0000

    V4.5

--- js/Soks.js
+++ js/Soks.js
@@ -3,29 +3,30 @@
     // const selectMaat = ;
     const Prijs = document.getElementById("prijs");
 
-    // selectQuantity.addEventListener("change", function() {
-    //     // Получаем выбранный вариант
-    //     const selectedOption1 = selectQuantity.options[selectQuantity.selectedIndex].value;
-
-    //     document.cookie = "Quantity=1; path=/";
-
-    // });
-    // selectMaat.addEventListener("change", function() {
-    //     // Получаем выбранный вариант
-    //     const selectedOption2 = selectMaat.options[selectMaat.selectedIndex].value;
-
-    //     // Выводим выбранный вариант в консоль
-    //     console.log("Выбран вариант:", nameSoks.innerHTML);
-    //     console.log("Выбран вариант:", selectedOption2);
-    // });
+    function getCookie(name) {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            if (cookie.startsWith(name + '=')) {
+                return cookie.substring(name.length + 1);
+            }
+        }
+        return "";
+    }
 
     function naarWinkelmand(){
         const selectQuantity = document.getElementById("quantity").options[document.getElementById("quantity").selectedIndex].value;
         const selectMaat = document.getElementById("size").options[document.getElementById("size").selectedIndex].value;
 
-        document.cookie = "GoederNaam="+nameSoks.innerHTML+"; path=/";
-        document.cookie = "Quantity="+selectQuantity+"; path=/";
-        document.cookie = "Maat="+selectMaat+"; path=/";
-        document.cookie = "Prijs="+Prijs.innerHTML+"; path=/";
-        console.log("Succses");
+        for(let i=0;i<=10;i++){
+            if(!getCookie("GoederNaam"+i) && !getCookie("Quantity"+i) &&
+            !getCookie("Maat"+i) && !getCookie("Prijs"+i)){
+                document.cookie = "GoederNaam"+i+"="+nameSoks.innerHTML+"; path=/";
+                document.cookie = "Quantity"+i+"="+selectQuantity+"; path=/";
+                document.cookie = "Maat"+i+"="+selectMaat+"; path=/";
+                document.cookie = "Prijs"+i+"="+Prijs.innerHTML+"; path=/";
+                return console.log("Succses");
+            }
+        }
+        
     }
--- js/cookie.js
+++ js/cookie.js
@@ -6,11 +6,26 @@ function getCookie(name) {
             return cookie.substring(name.length + 1);
         }
     }
+    
     return "";
 }
 
 if(document.getElementById("table-winkelmand")){
     const table_winkelmand = document.getElementById("table-winkelmand");
-    table_winkelmand.innerHTML += "<tr><td>"+getCookie("GoederNaam")+" "+getCookie("Maat")+
-    "</td><td>"+getCookie("Quantity")+"</td><td>"+getCookie("Prijs")+"</td></tr>";
+    let TOTAAL = 0;
+    let key = false;
+    for(let i=0;i<=10;i++){
+        if(getCookie("GoederNaam"+i) && getCookie("Quantity"+i) &&
+            getCookie("Maat"+i) && getCookie("Prijs"+i)){
+            key = true;
+            table_winkelmand.innerHTML += "<tr><td>"+getCookie("GoederNaam"+i)+" "+getCookie("Maat"+i)+
+            "</td><td>"+getCookie("Quantity"+i)+"</td><td>"+("€"+ (parseFloat((getCookie("Prijs"+i).replace("€", '')).replace(",", '.'))*parseInt(getCookie("Quantity"+i))).toFixed(2))+"</td></tr>";
+            TOTAAL += parseFloat((getCookie("Prijs"+i).replace("€", '')).replace(",", '.'))*parseInt(getCookie("Quantity"+i));
+        }   
+    }
+    key ? table_winkelmand.innerHTML += "<tr><th></th><th>Totaal:</th><th>€"+TOTAAL.toFixed(2)+"</th></tr>" : null;
+    if(document.getElementById("text-Niks")){
+        !key ? document.getElementById("text-Niks").style.display = "block" : document.getElementById("text-Niks").style.display = "none";
+    }
+    
 }
